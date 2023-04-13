02b3c44ca61e1e8add171bf09695b12ac3d0f4a7
William Wong <compulim@hotmail.com>
2023-04-12 21:38:00 -0700

    More debugging

--- src/index.js
+++ src/index.js
@@ -9,7 +9,41 @@ app.use('/', createStatic('./public'));
 
 app.use(
   '/.bot',
-  createProxyMiddleware({ target: 'https://webchat-mockbot3.azurewebsites.net/', changeOrigin: true, ws: true })
+  createProxyMiddleware({
+    target: 'https://webchat-mockbot3.azurewebsites.net/',
+    changeOrigin: true,
+    onProxyReqWs: (proxyReq, req, socket, options, head) => {
+      console.log('Connecting to DLASE', req.url);
+    },
+    ws: true
+  })
+);
+
+app.use(
+  ['/v3/directline/conversations', '/v3/directline/conversations/:id'],
+  createProxyMiddleware({
+    target: 'https://directline.botframework.com/',
+    changeOrigin: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, { statusCode }) => {
+      if (statusCode >= 200 && statusCode < 300) {
+        const body = responseBuffer.toString('utf8');
+        const json = JSON.parse(body);
+
+        if (json.streamUrl) {
+          json.streamUrl = json.streamUrl.replace(
+            /^wss:\/\/directline.botframework.com\/v3\/directline\//,
+            'ws://localhost:3000/v3/directline/'
+          );
+
+          return JSON.stringify(json);
+        }
+      }
+
+      return responseBuffer;
+    }),
+    selfHandleResponse: true,
+    ws: true
+  })
 );
 
 app.use(
@@ -17,22 +51,6 @@ app.use(
   createProxyMiddleware({
     target: 'https://directline.botframework.com/',
     changeOrigin: true,
-    onProxyRes: responseInterceptor(async (responseBuffer, { statusCode }, { method, url }, res) => {
-      if (statusCode >= 200 && statusCode < 300 && method === 'POST' && url === '/v3/directline/conversations') {
-        const body = responseBuffer.toString('utf8');
-        const json = JSON.parse(body);
-
-        json.streamUrl = json.streamUrl.replace(
-          /^wss:\/\/directline.botframework.com\/v3\/directline\//,
-          'ws://localhost:3000/v3/directline/'
-        );
-
-        return JSON.stringify(json);
-      }
-
-      return responseBuffer;
-    }),
-    selfHandleResponse: true,
     ws: true
   })
 );
